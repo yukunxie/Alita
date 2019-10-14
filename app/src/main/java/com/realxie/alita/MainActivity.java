f0e3b79a75f24f93ba8f014403a4bf5f6adaf747
xieyukun <xieyukun@yy.com>
2019-10-14 15:03:28 +0800

    rename vulkandemo to alita

--- app/src/main/java/com/realxie/alita/MainActivity.java
+++ app/src/main/java/com/realxie/alita/MainActivity.java
@@ -7,10 +7,13 @@ import android.content.res.AssetManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Surface;
-import android.widget.TextView;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String TAG = MainActivity.class.getSimpleName();
+
+    private VulkanSurfaceView vulkanSurfaceView_ = null;
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -26,10 +29,28 @@ public class MainActivity extends AppCompatActivity {
                 new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.INTERNET}, PackageManager.PERMISSION_GRANTED);
 
         // init vulkan context
-        VulkanSurfaceView surfaceView = findViewById(R.id.MainView);
-        initVulkanContextJNI(surfaceView.surfaceHolder.getSurface());
+        vulkanSurfaceView_ = findViewById(R.id.MainView);
+        initVulkanContextJNI(vulkanSurfaceView_.surfaceHolder.getSurface());
 
         setAssetManagerJNI((Context)this, getApplication().getAssets());
+    }
+
+    @Override
+    protected void onResume() {
+        Log.i(TAG, "onResume");
+        super.onResume();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        Log.i(TAG, "onWindowFocusChanged");
+        super.onWindowFocusChanged(hasFocus);
+    }
+
+    @Override
+    protected void onPause() {
+        Log.i(TAG, "onPause");
+        super.onPause();
     }
 
     /**
--- app/src/main/java/com/realxie/alita/VulkanSurfaceView.java
+++ app/src/main/java/com/realxie/alita/VulkanSurfaceView.java
@@ -2,7 +2,6 @@ package com.realxie.alita;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
